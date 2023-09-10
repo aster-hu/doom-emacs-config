31caa585d557e4025c915c9981ee8aaf5c3db4d4
astersbot <hasturhu@gmail.com>
2023-09-10 17:00:03 -0400

    Automated commit on 2023-09-10 17:00:03

--- config.el
+++ config.el
@@ -608,3 +608,33 @@ charset
 
 ;; Do not move one space left after leaving insert
 (setq evil-move-cursor-back nil)
+
+;; Smart input source
+
+  (use-package! sis
+  ;; :hook
+  ;; enable the /context/ and /inline region/ mode for specific buffers
+  ;; (((text-mode prog-mode) . sis-context-mode)
+  ;;  ((text-mode prog-mode) . sis-inline-mode))
+
+  :config
+  ;; For MacOS
+  (sis-ism-lazyman-config
+
+   ;; English input source may be: "ABC", "US" or another one.
+   ;; "com.apple.keylayout.ABC"
+   "com.apple.keylayout.Canadian"
+
+   ;; Other language input source: "rime", "sogou" or another one.
+   ;; "im.rime.inputmethod.Squirrel.Rime"
+   "im.rime.inputmethod.Squirrel.Hans")
+
+  ;; enable the /cursor color/ mode
+  (sis-global-cursor-color-mode t)
+  ;; enable the /respect/ mode
+  (sis-global-respect-mode t)
+  ;; enable the /context/ mode for all buffers
+  (sis-global-context-mode t)
+  ;; enable the /inline english/ mode for all buffers
+  (sis-global-inline-mode t)
+  )
