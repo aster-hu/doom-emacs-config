dd8ff53ffb2698b4bef34e005da18647a8f8471d
astersbot <hasturhu@gmail.com>
2023-09-16 01:00:02 -0400

    Automated commit on 2023-09-16 01:00:02

--- config.el
+++ config.el
@@ -705,20 +705,20 @@ charset
 ;; Set jk as the escape key
 (setq-default evil-escape-key-sequence "jk")
 
-;; (use-package! tabspaces
-;;   ;; use this next line only if you also use straight, otherwise ignore it. 
-;;   ;; :straight (:type git :host github :repo "mclear-tools/tabspaces")
-;;   :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
-;;   :commands (tabspaces-switch-or-create-workspace
-;;              tabspaces-open-or-create-project-and-workspace)
-;;   :custom
-;;   (tabspaces-use-filtered-buffers-as-default t)
-;;   (tabspaces-default-tab "Default")
-;;   (tabspaces-remove-to-default t)
-;;   (tabspaces-include-buffers '("*scratch*"))
-;;   ;; sessions
-;;   (tabspaces-session t)
-;;   (tabspaces-session-auto-restore t))
+(use-package! tabspaces
+  ;; use this next line only if you also use straight, otherwise ignore it. 
+  ;; :straight (:type git :host github :repo "mclear-tools/tabspaces")
+  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  ;; sessions
+  (tabspaces-session t)
+  (tabspaces-session-auto-restore t))
 
 
 ;; Quarto mode
@@ -731,3 +731,6 @@ charset
 
 ;; Disable cache to fix the warning error when previewing latex fragment
 ;; (setq org-element-use-cache nil)
+
+;; Perspective
+(use-package! perspective)
